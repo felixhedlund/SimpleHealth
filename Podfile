1af9a54ba7f4f976e25f52c1090a711fe321b095
Felix Hedlund <felixhedlund@me.com>
2016-09-13 01:09:52 +0200

    First commit

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.3'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'SimpleHealth' do
+    pod 'IBAnimatable'
+end
+
+target 'SimpleHealthTests' do
+
+end
+
+target 'SimpleHealthUITests' do
+
+end
+
---
+++ SimpleHealth/HelloViewController.swift
@@ -0,0 +1,40 @@
+//
+//  HelloViewController.swift
+//  SimpleHealth
+//
+//  Created by Felix Hedlund on 12/09/2016.
+//  Copyright © 2016 FelixHedlund. All rights reserved.
+//
+
+import UIKit
+
+class HelloViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let vc =  UIStoryboard(name: "Contents", bundle: nil).instantiateViewControllerWithIdentifier("Menu")
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
